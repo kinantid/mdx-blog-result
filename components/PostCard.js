9766e593ab2a834ae5ee4ab0f1db0c1a2a00170b
Kinanti Desyanandini <46942932+kinantid@users.noreply.github.com>
2024-09-25 21:57:19 +1000

    added text-subtle

--- components/PostCard.js
+++ components/PostCard.js
@@ -6,9 +6,9 @@ function PostCard({ title, date, description, slug, image }) {
       <a className="block my-4 hover:bg-gray-100">
         <img src={image} alt={title} className="w-full h-48 object-cover rounded-t" />
         <div className="p-4">
-          <h2 className="font-bold text-2xl my-4">{title}</h2>
-          <time className="text-gray-400">{date}</time>
-          <p className="mt-4 italic">{description}</p>
+          <h2>{title}</h2>
+          <time className="text-subtle">{date}</time>
+          <p className="text-subtle">{description}</p>
         </div>
       </a>
     </Link>
