0611d889a8d8b2c6fab8b711ed512f6dcb18e159
Kinanti Desyanandini <46942932+kinantid@users.noreply.github.com>
2024-09-25 22:24:00 +1000

    added card and layout-card-content

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 function PostCard({ title, date, description, slug, image }) {
   return (
     <Link href="/[slug]" as={`/${slug}`}>
-      <a className="block my-4 hover:bg-gray-100">
+      <a className="card">
         <img src={image} alt={title} className="w-full h-48 object-cover rounded-t" />
-        <div className="p-4">
+        <div className="layout-card-content">
           <h2>{title}</h2>
           <time className="text-subtle">{date}</time>
           <p className="text-subtle">{description}</p>
